7cac8ec0eb02b97d2313e4ffdcafeeb0145736e1
Jacob Säll Nilsson <jacob.e.s.nilsson@gmail.com>
2023-12-10 17:26:46 +0100

    fix setup script day argument

--- setup.sh
+++ setup.sh
@@ -23,10 +23,10 @@ else
   if test -f "inputs/$day.txt"; then
     echo "day already dowloaded, only updating puzzle.md"
     rm -rf puzzles/$day.md
-    aoc download -P -p puzzles/$day.md
+    aoc download -P -p puzzles/$day.md -d $day
   else
     echo "creating day $day"
-    aoc download -i inputs/$day.txt -p puzzles/$day.md
+    aoc download -i inputs/$day.txt -p puzzles/$day.md -d $day
     testData=$(ggrep -zPo '(?<=\`\`\`\n)[\s\S]*(?=\n\`\`\`)' puzzles/$day.md)
     touch $day.py
     printf "f = open(\"inputs/$day.txt\")\ndata = f.read().strip()\n\ntest_data = \"\"\"\n$testData\n\"\"\".strip()\n\ndef process(data):\n  return data\n\ndef part1(data):\n  return \"not implemented\"\n\ndef part2(data):\n  return \"not implemented\"\n\nprocessed = process(test_data)\nprint(\"part 1\", part1(processed))\nprint(\"part 2\", part2(processed))" >> $day.py
